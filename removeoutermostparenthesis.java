00e5f9c336bc24e73ee93536eeacfea6da6b6ab7
Anushka Dubey <136051483+anushkaa-dubey@users.noreply.github.com>
2025-04-05 23:42:09 +0530

    Create removeoutermostparenthesis.java

---
+++ removeoutermostparenthesis.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class P{
+    public String removeOuterParentheses(String s) {
+        StringBuilder result = new StringBuilder();
+        int count = 0;
+      if(count?
+         
+  }
+}
